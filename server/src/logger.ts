dc8ad3f59456dd5caea4431bd78c82ea1b01f7d9
Denyol18 <sprokdaniel@gmail.com>
2025-11-13 11:40:08 +0100

    graylog fixing continues

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,13 +1,31 @@
 import winston from "winston";
 import WinstonGraylog2 from "winston-graylog2";
 
+const GRAYLOG_HOST = "graylog";
+const GRAYLOG_PORT = 12201;
+
 const graylogTransport = new WinstonGraylog2({
   name: "Graylog",
   level: "info",
   graylog: {
-    servers: [{ host: "graylog", port: 12201 }],
+    servers: [{ host: GRAYLOG_HOST, port: GRAYLOG_PORT }],
+	hostname: "prf_server",
+	facility: "node_app",
+	bufferSize: 1400,
   },
   staticMeta: { service: "prf-server" },
+});
+
+graylogTransport.on("error", (error) => {
+  console.error("\x1b[31m[Graylog Transport Error]\x1b[0m", error.message);
+});
+
+graylogTransport.on("close", () => {
+  console.warn("\x1b[33m[Graylog Transport Closed]\x1b[0m");
+});
+
+graylogTransport.on("connect", () => {
+  console.log("\x1b[32m[Graylog Connected]\x1b[0m Sending logs to", GRAYLOG_HOST + ":" + GRAYLOG_PORT);
 });
 
 const logger = winston.createLogger({
@@ -22,6 +40,11 @@ const logger = winston.createLogger({
 	// @ts-expect-error
     graylogTransport,
   ],
+});
+
+logger.info("Logger initialized, attempting to connect to Graylog", {
+  graylogHost: GRAYLOG_HOST,
+  graylogPort: GRAYLOG_PORT,
 });
 
 export default logger;
