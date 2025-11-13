ff7ae4df9a81a5e6a0dc7b19fc0705f049f25a1d
Dániel Sprok <101126566+Denyol18@users.noreply.github.com>
2025-11-13 12:19:39 +0100

    removing unnecessary code

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -10,29 +10,10 @@ const graylogTransport = new WinstonGraylog2({
   graylog: {
     servers: [{ host: GRAYLOG_HOST, port: GRAYLOG_PORT }],
 	hostname: "prf_server",
-	facility: "node_app",
+	facility: "prf_app",
 	bufferSize: 1400,
   },
   staticMeta: { service: "prf-server" },
-});
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const graylogEmitter = graylogTransport as any;
-
-graylogEmitter.on("error", (error: any) => {
-  console.error("\x1b[31m[Graylog Transport Error]\x1b[0m", error.message);
-});
-
-graylogEmitter.on("close", () => {
-  console.warn("\x1b[33m[Graylog Transport Closed]\x1b[0m");
-});
-
-graylogEmitter.on("connect", () => {
-  console.log("\x1b[32m[Graylog Connected]\x1b[0m Sending logs to graylog:12201");
-});
-
-graylogEmitter.on("message", (msg: any) => {
-  console.debug("\x1b[36m[Graylog Message Sent]\x1b[0m", msg.short_message || msg.message);
 });
 
 const logger = winston.createLogger({
